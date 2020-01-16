052cd52e189e34bcbae834b74627bd757a0b5eca
brasoveanum <59613744+brasoveanum@users.noreply.github.com>
2020-01-16 16:18:35 -0500

    Add files via upload
    problem 2a final

--- gravitation.m
+++ gravitation.m
@@ -1,18 +1,20 @@
 %Input 
-for T = 90:10:130 ; 
+T = 90:10:130 ; 
 % Define constants: earth radius, G, earth mass
 R = 6.371*10^6; 
 G = 6.67*10^(-11); 
 M = 5.98*10^(24); 
 
 T = T*60; % convert to sec 
-h = (G*M*T^2/(4*pi^2))^(1/3) -R; %height in meters
+h = (G*M*(T.^2)/(4*pi^2)).^(1/3) -R; %height in meters
 h = h/1000
 
-for h = (G*M*T^2/(4*pi^2))^(1/3) -R; %height in meters
-v = sqrt((G*M)/(R+h))
+v = sqrt((G*M)./(R+h))
 v = round(v) 
 h = round(h)
-end
-end 
+x = table(v,h)
+
+
+
+ 
 
