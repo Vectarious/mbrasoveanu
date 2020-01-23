df344ec78d4b84e5f209d6b739ceaba1f0c06a22
brasoveanum <59613744+brasoveanum@users.noreply.github.com>
2020-01-22 22:17:03 -0500

    Add files via upload

---
+++ problem4.m
@@ -0,0 +1,19 @@
+load('smallperiodictable.mat')
+
+%find all densities greater than 1 g/cm^3
+n = names(density >1)
+size(n,1)
+
+%find all elements with isotopes over 100 
+e = names(isotopes > 100)
+size(e,1)
+
+%find number of elements discovered before 1900 
+discovery = names(discyear < 1900)
+size(discovery,1)
+
+%plot function that expresses relationship between atomic weight and density
+x = linspace(0,2*pi,45)
+y= ((x))
+plot(x,y)
+title('Density vs. Atomic Weight')
