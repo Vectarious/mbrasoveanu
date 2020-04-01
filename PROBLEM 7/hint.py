445e463800fd39639a0241bb1b11eabb6af1a42e
brasoveanum <59613744+brasoveanum@users.noreply.github.com>
2020-04-01 15:59:25 -0400

    Add files via upload

---
+++ PROBLEM 7/hint.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 15:57:59 2020
+
+@author: elfmaria
+"""
+
+def encode(x):
+    for i in range(0,len(y())):
+        GettysburgEncoded.append(codex[y()[i]])
+        return  
+    
+def decode(l,codex):
+    d_backwards = {}
+    for key, value in codex.items():
+        d_backwards(value) = key 
+        los = [d_backwards[num] for num in l] 
+        GettysburgDecoded = ''.join(los)
+        return 
